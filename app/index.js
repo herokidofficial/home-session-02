37adb92dbe09e26a9b218ebd2955b82e185f04de
Osariemen Daniel <Osariemen Daniel>
2017-03-14 12:06:46 +0100

    Add word count function.

--- app/index.js
+++ app/index.js
@@ -1,17 +1,36 @@
 'use strict'
 module.exports = {
-	/*function to return a hash of word counts*/
-	words: function(str) {
-		var word = str.replace(/\s\s+/g, ' ').split(/\s/);
-		var expectedCounts = {};
-		word.forEach(function(i) {
-			if (!expectedCounts[i]) {
-				expectedCounts[i] = 0;
+	words: function(wordCount) {
+		// split words argument into an array containing each of the words in it using any amount of whitespace as delimiter
+		var listOfWords = wordCount.split(/\s+/);
+
+		// Create an empty array to hold each of the entities in words without repitition
+		var uniqueWords = [];
+
+		listOfWords.forEach(function(word) {
+			if (uniqueWords.indexOf(word) !== 1) {
+				uniqueWords.push(word);
 			}
-			expectedCounts[i] += 1;
 		});
+
+		var expectedCounts = {};
+
+		for (var j of uniqueWords) {
+			var count = 0;
+			for (var i of listOfWords) {
+				// count how many times each of the words in uniqueWords appears in listOfWords
+				if (j === i) {
+					count += 1;
+				}
+			}
+
+			// create a key in expectedCounts with the value of j, each word in uniqueWords, and give
+			// it a property of count, the number of times it appears in listOfWords
+			expectedCounts[j] = count;
+		}
 		return expectedCounts;
 	},
+
 	/*reverse string function*/
 	reverseString: function(str) {
 		if (str === '') {
